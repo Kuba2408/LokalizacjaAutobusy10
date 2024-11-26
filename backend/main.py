ed7128c80d276e34d1a54ac86223c55fdbb881af
aleszczynskide <135127254+aleszczynskide@users.noreply.github.com>
2024-11-26 15:35:50 +0100

    First Localization

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+import requests
 
-app = FastAPI()
+def get_public_ip():
+    response = requests.get('https://ipinfo.io/ip')
+    return response.text.strip()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, TrackMyBus!"}
+def get_geolocation(ip_address):
+    url = f"https://ipinfo.io/{ip_address}/json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+ip = get_public_ip()
+location = get_geolocation(ip)
+
+if location:
+    print(f"IP: {location.get('ip')}")
+    print(f"Miasto: {location.get('city')}")
+    print(f"Region: {location.get('region')}")
+    print(f"Kraj: {location.get('country')}")
+    print(f"Lokalizacja: {location.get('loc')}")
+else:
+    print("Nie udało się uzyskać danych o lokalizacji.")
