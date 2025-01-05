332f30a9823e43130108ff52af528d2e69fc77a3
Czujsi <oskarbanaszczuk@gmail.com>
2025-01-05 18:52:16 +0100

    deleted comments

--- backend/api_wrapper/api_receiver.py
+++ backend/api_wrapper/api_receiver.py
@@ -1,17 +1,16 @@
 import warsaw_data_api
 import logging
 
-# Setting up logging configuration
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class ApiReceiver:
     def __init__(self, apikey):
         self.apikey = apikey
-        self.logger = logging.getLogger(__name__)  # Zainicjowanie loggera
+        self.logger = logging.getLogger(__name__)
 
     def bus_location(self):
-        self.logger.info(f"Fetching bus locations using API key: {self.apikey}")  # Dodanie logowania na początku
+        self.logger.info(f"Fetching bus locations using API key: {self.apikey}")
         try:
             ztm = warsaw_data_api.ztm(self.apikey)
             buses = ztm.get_buses_location()
