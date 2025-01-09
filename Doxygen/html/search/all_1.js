696ee529be48f6a9c4f7435e3eeb8ca55a878275
Bartosz Talarek <157415291+bartosztalarekdev@users.noreply.github.com>
2025-01-09 11:41:09 +0100

    Add files via upload

---
+++ Doxygen/html/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['get_5fall_5fdata_0',['get_all_data',['../namespacemain.html#a784199c561a19be4348d80742c63664a',1,'main']]],
+  ['get_5flocation_1',['get_location',['../namespacemain.html#a8ac5b0db4167742ed063538f3a21b773',1,'main']]],
+  ['get_5flocations_5fline_2',['get_locations_line',['../namespacemain.html#ab1563fd0207e4fa3cd5993cb004ca13b',1,'main']]]
+];
---
+++ Doxygen/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['startup_5fevent_0',['startup_event',['../namespacemain.html#a835ae097c78017008ff295695d4c62ee',1,'main']]]
+];
