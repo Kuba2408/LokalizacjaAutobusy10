2d623da256134eb32d301026ca7c322b78984c27
Jan Rudowski <jan.rudowski@icloud.com>
2025-01-09 20:37:31 +0100

    (feature): filter based on line, focus on bus, add line to the bus icon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@mantine/core/styles.css';
 import './index.css'
 import App from './App.tsx'
+import {MantineProvider} from "@mantine/core";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <MantineProvider>
     <App />
+      </MantineProvider>
   </StrictMode>,
 )
